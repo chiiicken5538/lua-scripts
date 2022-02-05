65737548daddc6f80c4c8b06e07d505424092a85
Chiiicken <65784006+Chiiicken5538@users.noreply.github.com>
2022-02-05 17:12:58 +0100

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -1,4 +1,11 @@
 local vu = game:GetService("VirtualUser")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "AntiAFK";
+    Text = "AntiAFK enabled - This will prevent Idle kicks";
+    Duration = 5;
+})
+
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    wait(1)

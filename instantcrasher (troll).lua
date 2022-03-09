5e5ccc672827bfc834b80ef877d3e5ea3df7b906
Chiiicken <65784006+Chiiicken5538@users.noreply.github.com>
2022-03-09 14:50:05 +0100

    Create instantcrasher (troll).lua

---
+++ instantcrasher (troll).lua
@@ -0,0 +1,64 @@
+--[[
+
+this is obv a troll
+this won't crash the server it just crashes your client
+Its not my problem if you lose any cash bcs your game crashed - I warned you.
+
+
+
+
+getgenv().instantCrash = true
+getgenv().crashingPower = 90
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiiicken5538/lua-scripts/main/instantcrasher (troll).lua"), true)()
+]]--
+
+-- script function (that will be never used :) )
+local instantCrash = true
+local crashingPower = 100
+
+
+local uwuwuwu = true
+local nononono = true
+local pizza = "hamburger"
+local sarma = "yummy"
+local spaghetti = "wow so yummy"
+local IamSmart = true
+local ILoveHorses = false
+local ikuOnTop = true
+local frozenYoghurt = "yummy"
+local sushi = "good"
+local noodles = "WOW SOO YUMMY UWUWUW"
+local sarma = "OMG WOW UWUWUWUWUW"
+local what = "??"
+local this = "is"
+local so = "op"
+local script = "^^"
+local DaHoodAntiCheat = "bad"
+local a = "b"
+local mcdonalds = "Burger king"
+local b = "c"
+local remindThisSentence = "whiletruedoendwhiletruedoend is so good."
+local howtogetfreeban = "twoMoreTimes"
+local yourpassword = "frerrobuxfurryuwu"
+local loggedCookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_ASDUHFIUDAHFIHASDIJOJFDINOIDHADHBSABVASOWEPORLKASDASDSDSD   i hve ur cookie pls  payy 1000million ewro to my paypal uwwu "
+local no = "yes"
+local wtf = "no??"
+local crashPackets = ":troll:"
+
+
+while true do 
+    crashServer()
+end
+-- method to crash server crashing server
+a = ""
+
+function crashServer()
+    if DaHoodAntiCheat == "enabled" then
+        -- abuse anticheat to crash server uwuwu
+        DaHoodAntiCheat = a..b..so..crashPackets..crashingPower..instantCrash..mcdonalds..remindThisSentence..noodles..a..b..so..script..wtf
+    end 
+end
+--[[
+yes, this code does not makes sense at all I could do that in one line but I am bored
+]]--

d3713fa86b3f5f4b484b55f35d353def00edf982
Chiiicken <65784006+Chiiicken5538@users.noreply.github.com>
2022-04-01 23:30:32 +0200

    Update instantcrasher (troll).lua

--- instantcrasher (troll).lua
+++ instantcrasher (troll).lua
@@ -6,26 +6,9 @@ Its not my problem if you lose any cash bcs your game crashed - I warned you.
 
 
 
-
-getgenv().isChickenHot = true
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiiicken5538/lua-scripts/main/instantcrasher (troll).lua"), true)()
 ]]--
 
--- this makes no sense
-local isChickenHot
-
-if isChickenHot then 
-  print("I love you")
-  print("anyways goodbye")
-else
-  print("fuck you :(((")
-end
 
 -- crash server ezzz jk
 while true do end
-
-
---[[
-yes, this code does not makes sense at all I could do that in one line but I am bored
-]]--

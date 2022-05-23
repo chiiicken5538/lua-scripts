c037fe96c2b52d4f4769b08447a9f23357ca1be2
Chiiicken <65784006+Chiiicken5538@users.noreply.github.com>
2022-05-23 12:10:12 +0200

    Update IkuRobloxAccountManagerIntegration.lua

--- IkuAddons/IkuRobloxAccountManagerIntegration.lua
+++ IkuAddons/IkuRobloxAccountManagerIntegration.lua
@@ -2,15 +2,26 @@ local localPlayer = game:GetService("Players").LocalPlayer
 
 
 --/ Roblox Account Manager API
-local RAMAccount = loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RAMAccount.lua')()
-local MyAccount = RAMAccount.new(game:GetService'Players'.LocalPlayer.Name)
+local RAMAccount = loadstring(game:HttpGet("https://raw.githubusercontent.com/Chiiicken5538/lua-scripts/main/IkuAddons/apis/RAMAccount.lua"))()
+local MyAccount = RAMAccount.new(localPlayer.Name)
 
+if getgenv().Settings.RAM_Integration == true then
+    if MyAccount then
+        print('[RAM-Integration] Successfully loaded Account ' .. localPlayer.display)
 
-
-if MyAccount then
-    print('[RAM-Integration] Successfully loaded Account ' .. localPlayer.display)
-
-    MyAccount:SetDescription("$".. localPlayer.DataFolder.Currency.Value)
+        MyAccount:SetDescription("$".. localPlayer.DataFolder.Currency.Value)
     
-    print('[RAM-Integration] Updated description ($'.. localPlayer.DataFolder.Currency.Value ..')')
+        print('[RAM-Integration] Updated description ($'.. localPlayer.DataFolder.Currency.Value ..')')
+    else
+        print('[RAM-Integration] Failed to load Account!')
+        print('[RAM-Integration] Please make sure that you have the following in the RAMSettings.ini file set:')
+        print('[RAM-Integration] ')
+        print('[RAM-Integration] --> EnableWebServer=true')
+        print('[RAM-Integration] --> WebServerPort=7963')
+        print('[RAM-Integration] --> AllowAccountEditing=true')
+        print('[RAM-Integration] --> Password=')
+        print('[RAM-Integration] --> EveryRequestRequiresPassword=false')
+        print('[RAM-Integration] ')
+        print('[RAM-Integration] You can find the RAMSettings.ini File in your Roblox Account Manager directory')
+    end
 end

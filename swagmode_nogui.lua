05bc781601d45df8c1560c9f4515b5c1d7660eb9
Chiiicken <65784006+Chiiicken5538@users.noreply.github.com>
2022-06-22 22:11:49 +0200

    Update swagmode_nogui.lua

--- swagmode_nogui.lua
+++ swagmode_nogui.lua
@@ -9,5 +9,7 @@ while wait() do
 			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].CrashFrame.Title:Destroy()
 			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].CrashFrame["Discord, which is discord.gg/Swaggest"]:Destroy()
 			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].CrashFrame.BackgroundTransparency = 1
+			
+			game:GetService("CoreGui").SwagmodeCrasher["made_by_Lerk#7643"].CrashFrame.Abuse.Position = UDim2.new(1,1, 1.2,0)
 		end
 end

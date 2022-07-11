dee1c0163eef5bb7880f1416bf99c24c0de18b7b
Chiiicken <65784006+Chiiicken5538@users.noreply.github.com>
2022-07-11 19:07:09 +0200

    Update DaHood RAM Integration.lua

--- DaHood RAM Integration.lua
+++ DaHood RAM Integration.lua
@@ -1,26 +1,24 @@
 --[[
-  
   This is a addon that sets your DaHood Cash as description in your Roblox Account Manager
 
     -> If you think I am cookie logging your accounts just read the code its fucking open source
 
-  made by chiiicken#3436 with the official RAM Account API 
+  made by chiiicken#3436 with the official RAM Account API
 
 ]]--
 
 --[[
             -- CHANGELOG --
-    
+
             07/11/2022 updated script loader: Script doesn't returns and throws error anymore when using autoexecute
 
-            -- CHANGELOG --       
+            -- CHANGELOG --
 ]]--
 
 --[[
 
 IF THE SCRIPT IS NOT WORKING THEN MAKE SURE TO COPY THE FOLLOWING SETTINGS IN YOUR "RAMSettings.ini" FILE !!!
-    
-    
+
 [WebServer]
 WebServerPort=7963
 AllowGetCookie=true
@@ -29,20 +27,22 @@ AllowLaunchAccount=true
 AllowAccountEditing=true
 EveryRequestRequiresPassword=false
 
-
 ]]--
 
 
-
 -- Script Loader
+wait(10) -- temporaly fix...
+repeat wait() until game.Players.LocalPlayer.Character
+print("[RAMStats] Waiting for DaHood to load...")
 
+--// second check
 DAHOOD_LOADED = false
 repeat wait()
 		if game:GetService("Players").LocalPlayer:FindFirstChild("DataFolder") ~= nil then
+				print("[RAMStats] Script ready.")
 				DAHOOD_LOADED = true
 		end
 until DAHOOD_LOADED == true
-
 
 
 -- Main script
@@ -53,21 +53,21 @@ local MyAccount = RAMAccount.new(game:GetService'Players'.LocalPlayer.Name)
 
 function format(amount)
     local formatted = amount
-    
-    while true do  
+
+    while true do
         formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1.%2')
         if (k==0) then
             break
         end
     end
-  
+
     return formatted
 end
 
 if MyAccount then
-    
-    print("Updated RAM description")
-    
+
+    print("[RAMStats] Updated RAM description")
+
     MyAccount:SetDescription("$"..format(game:GetService("Players").LocalPlayer.DataFolder.Currency.Value))
 
 end

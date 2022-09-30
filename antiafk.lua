8dc086809c95e2802a55df284363023f93381b55
chiiicken <65784006+chiiicken5538@users.noreply.github.com>
2022-09-30 10:05:43 +0200

    Delete antiafk.lua

--- antiafk.lua
+++
@@ -1,7 +0,0 @@
-local virtualUser = game:GetService("VirtualUser")
-
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-   virtualUser:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   wait(1)
-   virtualUser:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)

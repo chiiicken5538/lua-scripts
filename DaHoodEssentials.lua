2566e7391ab421d90e4271e4679837d5dccfe96a
chiiicken <65784006+chiiicken5538@users.noreply.github.com>
2023-01-07 18:28:51 +0100

    Create DaHoodEssentials.lua

---
+++ DaHoodEssentials.lua
@@ -0,0 +1,68 @@
+
+--[[
+
+getgenv().Settings = {
+    Macro = {
+        Enabled = true,
+    },
+
+    LowerGFX = {
+        Enabled = true
+    }
+}
+
+loadstring(game:HttpGet(""))()
+
+]]
+
+if getgenv().Settings.Macro.Enabled then
+    local macro = false
+
+    game:GetService("UserInputService").InputBegan:Connect(function(input)
+        if input.KeyCode == Enum.KeyCode.Q then
+            macro = true
+
+            repeat 
+                keypress(0x49)
+                game:GetService("RunService").Heartbeat:wait()
+                keypress(0x4F)
+                game:GetService("RunService").Heartbeat:wait()
+                keyrelease(0x49)
+                game:GetService("RunService").Heartbeat:wait()
+                keyrelease(0x4F)
+                game:GetService("RunService").Heartbeat:wait()
+                keypress(0x49)
+                game:GetService("RunService").Heartbeat:wait()
+                keypress(0x4F)
+                game:GetService("RunService").Heartbeat:wait()
+                keyrelease(0x49)
+                game:GetService("RunService").Heartbeat:wait()
+                keyrelease(0x4F)
+                game:GetService("RunService").Heartbeat:wait()
+            until not macro 
+        end
+    end)
+
+    game:GetService("UserInputService").InputEnded:Connect(function(input)
+        if input.KeyCode == Enum.KeyCode.Q then
+            macro = false
+        end
+    end)
+end
+
+-----------------------------------
+-----------------------------------
+
+if getgenv().Settings.LowerGFX.Enabled then
+    game:GetService("Lighting").FogEnd = 9e9
+    game:GetService("Lighting").Brightness = 0
+    game:GetService("Lighting").GlobalShadows = false
+
+
+    for i, v in pairs(game:GetDescendants()) do
+        if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+            v.Material = "Plastic"
+            v.Reflectance = 0
+        end
+    end
+end
